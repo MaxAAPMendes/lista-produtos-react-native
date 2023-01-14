976930e9f780fcf05ab9f2f7aab2ccbff188b3e0
MaxAAPMendes <max.sys.pinheiro@gmail.com>
2023-01-14 08:23:51 -0300

    feat: tratamento dos dados produtos favoritos

--- api/controllers/produtos.js
+++ api/controllers/produtos.js
@@ -16,31 +16,19 @@ export class Produtos {
       const produtos = await rest.get(`storeProducts?page=${pagina}`);
       console.log("lista de produtos", produtos);
       const {
-        status,
         data,
         statusText
       } = produtos;
       return {
         status: "sucesso",
-        statusCode: status,
         mensagem: statusText,
         dados: data
       };
     } catch (erro) {
       console.log("Erro ao consultar produtos", erro);
-      const { response } = erro;
-      let status = null;
-      let data = null;
-      if (response) {
-        status = response.status;
-        data = response.data;
-      }
-      // const { status, data } = erro?.response;
       return {
         status: "erro",
-        statusCode: status || 500,
-        mensagem: data ? data.message : "Erro ao consultar produtos",
-        dados: erro
+        mensagem: erro.message || "Erro ao consultar produtos",
       };
     }
   }
@@ -50,19 +38,19 @@ export class Produtos {
     try {
       const favoritos = await rest.get('storeProducts/getFavProducts');
       console.log(favoritos);
+      let listaFavoritos;
+      if (favoritos && favoritos.status === 200) {
+        listaFavoritos = favoritos.data.products || [];
+      }
       return {
         status: "sucesso",
-        statusCode: 'favoritos.status',
-        mensagem: 'favoritos.statusText',
-        dados: favoritos.data.products
+        dados: listaFavoritos
       }
     } catch(erro) {
       console.log("Erro a consulta favoritos", erro);
       return {
         status: "erro",
-        statusCode: 500,
-        mensagem: data ? data.message : "Erro ao consultar produtos",
-        dados: erro
+        mensagem: erro.message || "Erro ao consultar produtos favoritos",
       }
     }
   }
